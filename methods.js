40039f5acd57b90a01002e824f95dd211b972ecb
ama22ni <astroamanii@gmail.com>
2022-05-07 21:58:42 +0300

    add things

---
+++ methods.js
@@ -0,0 +1,127 @@
+function showEhtetab() {
+    for (let x in Ehtetab) {
+        Ehtetab[x].addTo(map);
+               }
+               for (let x in Raei) {
+                Raei[x].removeFrom(map);
+                       }
+                       for (let x in Said) {
+                        Said[x].removeFrom(map);
+                       }
+                       for (let x in Eshaal) {
+                        Eshaal[x].removeFrom(map);
+                       }
+                       for (let x in Naqel) {
+                        Naqel[x].removeFrom(map);
+                       }
+                       for (let x in Balag) {
+                        Balag[x].removeFrom(map);
+                       }
+            }
+
+                    function showraei() {
+                        for (let x in Raei) {
+                            Raei[x].addTo(map);
+               }
+                        for (let x in Ehtetab) {
+                Ehtetab[x].removeFrom(map);
+                       }
+                       for (let x in Said) {
+                        Said[x].removeFrom(map);
+                       }
+                       for (let x in Eshaal) {
+                        Eshaal[x].removeFrom(map);
+                       }
+                       for (let x in Naqel) {
+                        Naqel[x].removeFrom(map);
+                       }
+                       for (let x in Balag) {
+                        Balag[x].removeFrom(map);
+                       }
+                    }
+
+                    function showSaid() {
+                        for (let x in Said) {
+                            Said[x].addTo(map);
+               }
+                        for (let x in Ehtetab) {
+                Ehtetab[x].removeFrom(map);
+                       }
+                       for (let x in Raei) {
+                        Raei[x].removeFrom(map);
+                       }
+                       for (let x in Eshaal) {
+                        Eshaal[x].removeFrom(map);
+                       }
+                       for (let x in Naqel) {
+                        Naqel[x].removeFrom(map);
+                       }
+                       for (let x in Balag) {
+                        Balag[x].removeFrom(map);
+                       }
+                    }
+
+
+                    function showEshaal() {
+                        for (let x in Eshaal) {
+                            Eshaal[x].addTo(map);
+               }
+                        for (let x in Ehtetab) {
+                Ehtetab[x].removeFrom(map);
+                       }
+                       for (let x in Raei) {
+                        Raei[x].removeFrom(map);
+                       }
+                       for (let x in Said) {
+                        Said[x].removeFrom(map);
+                       }
+                       for (let x in Naqel) {
+                        Naqel[x].removeFrom(map);
+                       }
+                       for (let x in Balag) {
+                        Balag[x].removeFrom(map);
+                       }
+                    }
+
+
+                    function showNaqel() {
+                        for (let x in Naqel) {
+                            Naqel[x].addTo(map);
+               }
+                        for (let x in Ehtetab) {
+                Ehtetab[x].removeFrom(map);
+                       }
+                       for (let x in Raei) {
+                        Raei[x].removeFrom(map);
+                       }
+                       for (let x in Said) {
+                        Said[x].removeFrom(map);
+                       }
+                       for (let x in Eshaal) {
+                        Eshaal[x].removeFrom(map);
+                       }
+                       for (let x in Balag) {
+                        Balag[x].removeFrom(map);
+                       }
+                    }
+
+                    function showBalag() {
+                        for (let x in Naqel) {
+                            Naqel[x].addTo(map);
+               }
+                        for (let x in Ehtetab) {
+                Ehtetab[x].removeFrom(map);
+                       }
+                       for (let x in Raei) {
+                        Raei[x].removeFrom(map);
+                       }
+                       for (let x in Said) {
+                        Said[x].removeFrom(map);
+                       }
+                       for (let x in Eshaal) {
+                        Eshaal[x].removeFrom(map);
+                       }
+                       for (let x in Balag) {
+                        Balag[x].removeFrom(map);
+                       }
+                    }
